3e9cf74b5e3955915dc2547d2afd6fd6201ca5cb
elisa <elisa.terrana@mail.polimi.it>
2024-06-04 12:20:02 +0000

    Solve access to executable from mpirun 5

---
+++ job1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=step2TerranaZechini1
+#SBATCH --output=output1.txt
+#SBATCH --error=error1.txt
+#SBATCH --ntasks=2
+#SBATCH --time=00:05:00
+
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+singularity run -c --bind /scratch_local:$TMPDIR Singularity1.sif
--- singularity-3.8.3/pkg/util/loop/loop_no_singularity.go
+++
@@ -1,9 +0,0 @@
-// +build !singularity_engine
-
-package loop
-
-// GetMaxLoopDevices Return the maximum number of loop devices allowed
-func GetMaxLoopDevices() int {
-	// externally imported package, use the default value
-	return 256
-}
