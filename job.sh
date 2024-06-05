98157443a880e9fe22a3d10d54f4f2eb3015bd17
MatteoZechini <145035060+MatteoZechini@users.noreply.github.com>
2024-06-05 13:24:15 +0200

    Fine progetto (#1)
    * inizio job.sh
    
    * 3/6
    
    * step 2 completato?
    
    * step 2 ok
    
    * build/main
    
    * mpirun outside
    
    * mpirun inside
    
    * fine step 2 si spera
    
    * adesso si
    
    * moved singularity install
    
    * -f -R
    
    * step 3 finito? si spera
    
    * prova senza --bind
    
    * FINE
    
    * FINE AUTOMATICO
    
    * FINE FINE
    
    ---------
    
    Co-authored-by: elisa <elisa.terrana@mail.polimi.it>

---
+++ job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=step2TerranaZechini
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+#SBATCH --ntasks=2
+#SBATCH --time=00:10:00
+
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+module load singularity
+module load openmpi
+
+singularity run --bind /scratch_local:$TMPDIR Singularity.sif
